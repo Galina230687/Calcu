c3ee8b51bca702c7d1cda21bdd83aee8ab37b229
Galina <serena_05@mail.ru>
2023-02-09 09:18:43 +0300

    new commit1

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,7 +9,7 @@ public interface Calculator {
     BinaryOperator<Integer> plus = Integer::sum;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x , y) -> (x == 0) ? 0 : (x / y);
+    BinaryOperator<Integer> devide = (x , y) -> (y == 0) ? 0 : (x / y);
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
